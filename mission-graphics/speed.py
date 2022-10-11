6ac845b2d4bcfc0cf1b7ab0a61c85721e6481bdf
srosati <srosati@itba.edu.ar>
2022-10-11 12:11:13 -0300

    feat: Calculate mission time

--- mission-graphics/speed.py
+++ mission-graphics/speed.py
@@ -4,10 +4,15 @@ import math
 import numpy as np
 
 min_date = '2023-05-08T21:30'
-dt = 30000
+dt = 300
 
+steps_per_day = math.ceil(24 * 60 * 60 / dt)
+print("Steps per day:", steps_per_day)
+print()
+
+total_days = 0
 speeds = []
-
+day_steps = 1
 with open("../sweep_mission_out.txt", "r") as mission_file:
     line = mission_file.readline()
     while line[:-1] != min_date:
@@ -24,15 +29,27 @@ with open("../sweep_mission_out.txt", "r") as mission_file:
             if id == 3:
                 speeds.append(math.sqrt(float(vx) ** 2 + float(vy) ** 2))
 
-
+        if (day_steps % steps_per_day == 0):
+            total_days += 1
+            day_steps = 0
+        
+        day_steps += 1
+        
         line = mission_file.readline()
 
 mission_file.close()
 
-print((len(speeds)+1) * dt)
+
+time_elapsed = dt * day_steps
+
+hours = int(time_elapsed / 3600)
+minutes = int((time_elapsed - hours * 3600) / 60)
+
+# ESTO SOLO DA BIEN SI NO SE SALTEAN ITERACIONES AL ESCRIBIR, SINO VA A DAR SIEMPRE UN POCO DE MAS
+print(total_days, hours, minutes) 
 
 plt.plot(speeds)
-xticks = np.arange(0, len(speeds), step=10)
+xticks = np.arange(0, len(speeds), step=int(len(speeds) / 20))
 plt.xticks(xticks, [i * dt for i in xticks], rotation=45)
 plt.xlabel("Tiempo (s)")
 plt.ylabel("Modulo de la Velocidad (km / s)")
